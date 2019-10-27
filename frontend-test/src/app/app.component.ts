a2c9c4e3ab19b93833eb9e27b997d9054e4813b5
amitlap <39777233+amitlap@users.noreply.github.com>
2019-10-27 15:29:39 +0200

    Update app.component.ts

--- frontend-test/src/app/app.component.ts
+++ frontend-test/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Subject } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Tpredic-Gallery-Test';
